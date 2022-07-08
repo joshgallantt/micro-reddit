92f3bb426c09e5c18f127cea44569feb0458d63c
Josh Gallant <joshgallant78@gmail.com>
2022-07-08 23:26:48 +0100

    card tweaks

--- src/features/Post/Post.jsx
+++ src/features/Post/Post.jsx
@@ -30,22 +30,30 @@ export default function Post() {
 
   return (
     <div className="post">
-      <div className="post--container">
-        <div className="post--container--votebar">
-          <div className="upvotes">{data.data.children[0].data.ups}</div>
+      <div className="post-card">
+        <h2>{data.data.children[0].data.title}</h2>
+        <div className="post--container">
+          <div className="post--container--votebar">
+            <div className="up">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
+                <path d="M374.6 246.6C368.4 252.9 360.2 256 352 256s-16.38-3.125-22.62-9.375L224 141.3V448c0 17.69-14.33 31.1-31.1 31.1S160 465.7 160 448V141.3L54.63 246.6c-12.5 12.5-32.75 12.5-45.25 0s-12.5-32.75 0-45.25l160-160c12.5-12.5 32.75-12.5 45.25 0l160 160C387.1 213.9 387.1 234.1 374.6 246.6z" />
+              </svg>
+            </div>
+            <h3 className="score">{data.data.children[0].data.ups}</h3>
+            <div className="down">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
+                <path d="M374.6 310.6l-160 160C208.4 476.9 200.2 480 192 480s-16.38-3.125-22.62-9.375l-160-160c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0L160 370.8V64c0-17.69 14.33-31.1 31.1-31.1S224 46.31 224 64v306.8l105.4-105.4c12.5-12.5 32.75-12.5 45.25 0S387.1 298.1 374.6 310.6z" />
+              </svg>
+            </div>
+          </div>
+          <div className="post--container--content">
+            <div>{preview()}</div>
+            <div>{data.data.children[0].data.selftext}</div>
+          </div>
         </div>
-
-        <div className="post--container--content">
-          <div className="post--container--content-title">
-            {data.data.children[0].data.title}
-          </div>
-          <div>{preview()}</div>
-          <div>{data.data.children[0].data.selftext}</div>
-          <div className="post--container--content-bottominfo">
-            <div>{data.data.children[0].data.subreddit_name_prefixed}</div>
-            <div>{data.data.children[0].data.num_comments} comments</div>
-            <div>{data.data.children[0].data.author}</div>
-          </div>
+        <div className="post--container--content-bottominfo">
+          <h4>{data.data.children[0].data.num_comments} comments</h4>
+          <h4>Posted by u/{data.data.children[0].data.author}</h4>
         </div>
       </div>
     </div>
