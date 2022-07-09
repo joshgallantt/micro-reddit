4810656c5d350d1fe18918870b584ced0b446b3b
Josh Gallant <joshgallant78@gmail.com>
2022-07-09 11:52:15 +0100

    layout changes

--- src/features/Post/Post.jsx
+++ src/features/Post/Post.jsx
@@ -6,22 +6,22 @@ export default function Post(props) {
   console.log(data);
 
   const preview = () => {
-    if (data.data.children[0].data.secure_media !== null) {
+    if (data.data.children[12].data.secure_media !== null) {
       return (
         <video controls className="preview">
           <source
             src={
-              data.data.children[0].data.secure_media.reddit_video.fallback_url
+              data.data.children[12].data.secure_media.reddit_video.fallback_url
             }
             type="video/mp4"
           />
         </video>
       );
-    } else if (data.data.children[5].data.url != null) {
+    } else if (data.data.children[6].data.url != null) {
       return (
         <img
           className="preview"
-          src={data.data.children[5].data.url}
+          src={data.data.children[12].data.url}
           alt="thumb"
         />
       );
@@ -58,7 +58,16 @@ export default function Post(props) {
           </div>
         </div>
         <div className="post--container--content-bottominfo">
-          <h4>{data.data.children[0].data.num_comments} comments</h4>
+          <div className="comment">
+            <div className="comment--count">
+              {data.data.children[0].data.num_comments}
+            </div>
+            <div className="comment--icon">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                <path d="M256 32C114.6 32 .0272 125.1 .0272 240c0 47.63 19.91 91.25 52.91 126.2c-14.88 39.5-45.87 72.88-46.37 73.25c-6.625 7-8.375 17.25-4.625 26C5.818 474.2 14.38 480 24 480c61.5 0 109.1-25.75 139.1-46.25C191.1 442.8 223.3 448 256 448c141.4 0 255.1-93.13 255.1-208S397.4 32 256 32zM256.1 400c-26.75 0-53.12-4.125-78.38-12.12l-22.75-7.125l-19.5 13.75c-14.25 10.12-33.88 21.38-57.5 29c7.375-12.12 14.37-25.75 19.88-40.25l10.62-28l-20.62-21.87C69.82 314.1 48.07 282.2 48.07 240c0-88.25 93.25-160 208-160s208 71.75 208 160S370.8 400 256.1 400z" />
+              </svg>
+            </div>
+          </div>
           <h4>Posted by u/{data.data.children[0].data.author}</h4>
         </div>
       </div>
